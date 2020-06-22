19d20620188a355363810a681963a2a1e5f21502
Mr.Lin <linsongbin@126.com>
2020-06-22 13:25:45 +0800

    add Gateway

---
+++ src/MqttFx.Gateway/Sessions/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MqttFx.Gateway.Sessions
+{
+    public class Session
+    {
+        public Session(Socket client)
+        {
+            Client = client;
+        }
+
+        public Socket Client { get; set; }
+    }
+}

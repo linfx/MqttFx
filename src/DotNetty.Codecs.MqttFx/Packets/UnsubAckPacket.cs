b4ee058984826e2cbc6833306ddda0830c603374
Mr.Lin <linsongbin@126.com>
2022-03-05 20:59:55 +0800

    UnsubAckPacket

--- src/DotNetty.Codecs.MqttFx/Packets/UnsubAckPacket.cs
+++ src/DotNetty.Codecs.MqttFx/Packets/UnsubAckPacket.cs
@@ -1,7 +1,8 @@
 ﻿namespace DotNetty.Codecs.MqttFx.Packets
 {
     /// <summary>
-    /// 取消订阅回执
+    /// 取消订阅回执(UNSUBACK – Unsubscribe acknowledgement)
+    /// The UNSUBACK Packet is sent by the Server to the Client to confirm receipt of an UNSUBSCRIBE Packet.
     /// </summary>
     public sealed class UnsubAckPacket : PacketWithId
     {
